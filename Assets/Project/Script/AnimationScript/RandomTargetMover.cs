a67d5131a75f0cb7526a1f9a54b1f0303ee65de0
ngvha0510 <Ngvha0510@gmail.com>
2024-01-21 10:31:53 +0700

    AiEnemyHealth

---
+++ Assets/Project/Script/AnimationScript/RandomTargetMover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomTargetMover : MonoBehaviour
+{
+    [SerializeField] float _radius = 2000f;
+    [SerializeField] int _delay = 10;
+
+    void OnEnable()
+    {
+        InvokeRepeating(nameof(MoveToRandomLocation), 0, _delay);
+    }
+
+    void MoveToRandomLocation()
+    {
+        transform.position = Random.insideUnitSphere * _radius;
+    }
+}
